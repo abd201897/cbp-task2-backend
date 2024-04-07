16674262547fa5667ac085d898610b5e7afd1940
HurtedHacker1@ <Hamzaakramkhan11112@gmail.com>
2024-04-07 23:11:38 +0500

    Models are defined for:  - User extended with built-in User model with customized fields.  - Course and Modules models are defined.

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from utils.models import Roles
+
+
 # Create your models here.
 
 
 class User(AbstractUser):
+    role = models.ForeignKey(Roles, default=None, null=True, on_delete=models.SET_NULL)
+    date_of_birth = models.BigIntegerField(default=None, null=False, verbose_name='Date Of Birth')
+    address = models.TextField(default='', null=False, verbose_name='Address')
+    city = models.CharField(max_length=100, default='', null=False, verbose_name='City')
+    country = models.CharField(max_length=100, default='', null=False, verbose_name='Country')
+    image = models.TextField(default='', verbose_name='Profile Picture URL')
 
     def __str__(self):
         return f"{self.get_full_name()} [Username: {self.username}] | [Role: {self.role}]"
 
     class Meta:
         db_table = "User"
+
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,51 @@
 from django.db import models
-
+from utils.utils import get_code
 # Create your models here.
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=100, null=True, verbose_name='Category Name')
+
+
+class Module(models.Model):
+    """
+    Modules Model that can have multiple courses init.
+    """
+    AVAILABILITY_CHOICES = [
+        ('open', 'Open for registration'),
+        ('closed', 'Closed for registration'),
+    ]
+
+    name = models.CharField(max_length=255, verbose_name='Name of the module')
+    code = models.CharField(max_length=50, unique=True, verbose_name='Code')
+    credit = models.IntegerField(verbose_name='Credit')
+    category = models.CharField(max_length=100, verbose_name='Category')
+    description = models.TextField(verbose_name='Description')
+    availability = models.CharField(
+        max_length=10,
+        choices=AVAILABILITY_CHOICES,
+        default='open',
+        verbose_name='Availability'
+    )
+    course_allowed = models.ManyToManyField('Course', related_name='modules', verbose_name='Courses allowed to register')
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+    def save(self, *args, **kwargs):
+
+        self.code = f"{self.name}/{get_code()}"
+
+        super().save(*args, **kwargs)
+
+
+class Course(models.Model):
+    """
+    Represents a course that can have multiple modules offered.
+    """
+    name = models.CharField(max_length=255, verbose_name='Name of the course')
+    code = models.CharField(max_length=50, unique=True, verbose_name='Code')
+    description = models.TextField(verbose_name='Description')
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
