8ca354919ced7cfe379d7d4f3a769877c48af182
jigar9696 <jigar9696@gmail.com>
2024-04-22 11:46:12 +0500

    Course pages populated with the list of modules.

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from courses.views import get_courses_list, get_course_modules
 
 urlpatterns = [
+    # ### Course List API's
+    path('get_courses_list', get_courses_list, name='get_courses_list'),
+    path('get_course_modules', get_course_modules, name='get_course_modules'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+from courses.models import Course
+from courses.serializers import CourseSerializer
+from utils.handlers.request_handlers import DRHandler
+
+DR_handler = DRHandler()
 
 # Create your views here.
+
+
+@DR_handler.public_rest_call(allowed_methods=['GET'])
+def get_courses_list(request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 10  # Set the number of items per page
+    courses = Course.objects.all()
+    result_page = paginator.paginate_queryset(courses, request)
+    serializer = CourseSerializer(result_page, many=True)
+    return paginator.get_paginated_response(serializer.data)
+
+
+@DR_handler.public_rest_call(allowed_methods=['GET'])
+def get_course_modules(request):
+    course_id = request.GET.get('id', None)
+    if course_id and type(int(course_id)) == int:
+        paginator = PageNumberPagination()
+        paginator.page_size = 10  # Set the number of items per page
+        course_ = Course.objects.filter(id=course_id)
+        result_page = paginator.paginate_queryset(course_, request)
+        serializer = CourseSerializer(result_page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+    else:
+        return Response(data={
+            'message': '`course_id` of type int be provided.'
+        }, status=status.HTTP_400_BAD_REQUEST)
