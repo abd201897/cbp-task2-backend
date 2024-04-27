8f618898f26207ebcc3b7f7bff4569f982beb078
Muhammad Abdullah Aslam <85877524+abd201897@users.noreply.github.com>
2024-04-27 10:53:20 +0000

    added pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
-echo $environment > .env
+echo $environment > .env.example
+sed 's/ \([^ ]*=\)/\n\1/g' .env.example > .env
 service ssh start
-#python manage.py makemigrations
-#python manage.py migrate 
-#exec python manage.py runserver 0.0.0.0:80
-tail -f /dev/null
+python manage.py makemigrations
+python manage.py migrate 
+exec python manage.py runserver 0.0.0.0:80
